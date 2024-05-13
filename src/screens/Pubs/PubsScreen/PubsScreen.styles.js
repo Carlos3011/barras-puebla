c82bf666db4793d18c4f5bba7584923477906dbd
Garcia <carlos.garcia.diaz2000@hotmail.com>
2024-05-13 00:44:26 -0600

    Boton adaptativo completado

--- src/screens/Pubs/PubsScreen/PubsScreen.styles.js
+++ src/screens/Pubs/PubsScreen/PubsScreen.styles.js
@@ -1,13 +1,15 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    content: {
-        flex: 1,
-    },
-    btnContainer: {
-        top: 700,
-        position: 'absolute',
-        bottom: 10,
-        right: 10,
-    },
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+  },
+  btnContainer: {
+    position: "absolute",
+    bottom: 16,
+    right: 16,
+  },
 });
